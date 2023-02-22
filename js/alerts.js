e36e238504bfebd04dee1e96fa055aba4b7a57bd
JohnoCZ <jonas.zidek@seznam.cz>
2023-02-22 10:55:59 +0100

    Added langs & projects

--- js/alerts.js
+++ js/alerts.js
@@ -2,10 +2,7 @@ function open_alert(name) {
     if(name === "markusfilm") {
         Swal.fire({
             title: '<strong>Markusfilm.com</strong>',
-            html: '<p>Web je napsaný ve framework Laravel, databáze běží na SQL. ' +
-                'Mojí hlavní náplní práce je vyvíjení nových funkcí pro casting, ' +
-                'který se nachází na webu. Dále upravuji již zavedené funkce a ' +
-                'udržuji vše funkční.</p>',
+            html: document.getElementById('alert.markusfilm').innerText,
             showCloseButton: false,
             showCancelButton: false,
             focusConfirm: false,
@@ -15,9 +12,7 @@ function open_alert(name) {
     else if (name === "chapes") {
         Swal.fire({
             title: '<strong>Chápeš.cz</strong>',
-            html: '<p>Web běží na jazyce PHP a dále je osazen o jazyky JavaScript, ' +
-                'CSS, HTML a knihovnu stylů Bootstrap. Mojí hlavní náplní práce je upravování ' +
-                'již zavedených funkcí či přidávání nových vylepšení + udržuji vše funkční.</p>',
+            html: document.getElementById('alert.chapes').innerText,
             showCloseButton: false,
             showCancelButton: false,
             focusConfirm: false,
@@ -27,26 +22,7 @@ function open_alert(name) {
     else if (name === "drip") {
         Swal.fire({
             title: '<strong>DRIP</strong>',
-            html: '<p>Mojí hlavní náplní práce je vedení IT teamu společně s grafickým teamem. ' +
-                'Dále vyvíjím eshop pro prodej dřevěných výrobků, kde si uživatel může ' +
-                'sám navrhnout grafiku, která bude na produkt vypálená. Web beží na jazyce ' +
-                'PHP a na front-end byly použity jazyky JavaScript, HTML, CSS a knihovna ' +
-                'stylů Bootstrap.</p>' +
-                '<p>22.11.2022: Tento den jsem již pouze manažer IT teamu a kontroluji ' +
-                'vývoj nové verze E-Shopu psaného v JavaScriptu (React, TypeScript, Nodejs).</p>',
-            showCloseButton: false,
-            showCancelButton: false,
-            focusConfirm: false,
-            confirmButtonText: 'Zavřít',
-        })
-    }
-    else if (name === "youtube") {
-        Swal.fire({
-            title: '<strong>YouTube Downloader</strong>',
-            html: '<p>Kamarád mě kdysi poprosil o jednoduchou Python aplikaci na automatické ' +
-                'stahování podle zkopírovaného odkazu na YouTube. Do budoucnosti se chystám tuto ' +
-                'aplikaci přepsat a přidat jí jednoduchou instalaci a GUI prostředí ' +
-                'pro lehčí uživatelské používání.</p>',
+            html: document.getElementById('alert.drip').innerText,
             showCloseButton: false,
             showCancelButton: false,
             focusConfirm: false,
@@ -56,12 +32,7 @@ function open_alert(name) {
     else if (name === "paspal") {
         Swal.fire({
             title: '<strong>PASPAL</strong>',
-            html: '<p>Po nástupu na střední školu jsem se zapojil společně s 5 spolužáky ' +
-                'do souteže CanSat, kde jsme měli za úkol vytvořit satelit velikosti plechovky. ' +
-                'Mojí hlavní náplní práce bylo vytváření webových stránek pro propagaci teamu a ' +
-                '"živého" webu pro zobrazení aktuálních dat ze satelitu v realném čase. Hlavní web ' +
-                'byl napsaný v jazyce HTML a CSS společně s knihovnou stylů W3.CSS a "živy" web byl ' +
-                'napsaný v jazyke PHP a následně přepsaný do Node.js.</p>',
+            html: document.getElementById('alert.paspal').innerText,
             showCloseButton: false,
             showCancelButton: false,
             focusConfirm: false,
@@ -71,16 +42,21 @@ function open_alert(name) {
     else if (name === "traiderblocks") {
         Swal.fire({
             title: '<strong>TraiderBlocks.eu</strong>',
-            html: '<p>V roce 2019 jsem byl přizvaný do jednoho MC projektu, ' +
-                'kde jsem měl tvořit webové strány a spravovat pluginy na serverech. ' +
-                'Tento projekt však rychle zaniknul a celý tým jsem vzal pod nový projekt ' +
-                'TraiderBlocks.eu. Mojí náplní práce bylo vést celý team a spravovat linuxové servery ' +
-                'společně s webovými stránkami a databázovým systémem MariaDB. V dnešní době je projekt ' +
-                'neaktivní, jeho plánovaná obnova je v polovině roku 2023.</p>',
+            html:  document.getElementById('alert.traiderblocks').innerText,
+            showCloseButton: false,
+            showCancelButton: false,
+            focusConfirm: false,
+            confirmButtonText: 'Zavřít',
+        })
+    }else if (name === "livezone") {
+        Swal.fire({
+            title: '<strong>LiveZone.cz</strong>',
+            html: document.getElementById('alert.livezone').innerText,
             showCloseButton: false,
             showCancelButton: false,
             focusConfirm: false,
             confirmButtonText: 'Zavřít',
         })
     }
+
 }
--- js/lang.js
+++ js/lang.js
@@ -1,21 +1,53 @@
-import { lang } from "../lang/cs.js"
-export function switch_lang(selected_lang = null) {
-    // Check if lang is on the list and import lang file
-    if(selected_lang == "en") {
-        // Translate content
-        translate()
+function Translate() {
+    //initialization
+    this.init =  function(attribute, lng){
+        this.attribute = attribute;
+        this.lng = lng;
     }
+    //translate
+    this.process = function(){
+        _self = this;
+        let xrhFile = new XMLHttpRequest();
+        //load content data
+        xrhFile.open("GET", "lang/"+this.lng+".json", false);
+        xrhFile.onreadystatechange = function ()
+        {
+            if(xrhFile.readyState === 4)
+            {
+                if(xrhFile.status === 200 || xrhFile.status == 0)
+                {
+                    let LngObject = JSON.parse(xrhFile.responseText);
+                    let allDom = document.getElementsByTagName("*");
+                    for(
+                        let i =0; i < allDom.length; i++){
+                        let elem = allDom[i];
+                        let key = elem.getAttribute(_self.attribute);
+                        if(key != null) {
+                            elem.innerHTML = LngObject[key]  ;
+                        }
+                    }
 
-    /*
-        This function is for translating original content to the
-        other lang from variable $lang form lang file.
-     */
-    function translate() {
-        for (const [key, value] of Object.entries(lang)) {
-            const elements = document.querySelectorAll('[data-id=' + key +']');
-            for (const element of elements) {
-                element.innerHTML = value;
+                }
             }
         }
+        xrhFile.send();
     }
 }
+
+//This function will be called when user click changing language
+function translate(lng){
+    let translate = new Translate();
+    translate.init('data-lang', lng);
+    translate.process();
+}
+
+$(document).ready(function(){
+    //This is id of HTML element (English) with attribute lng-tag
+    $("#enTranslator").click(function(){
+        translate('en');
+    });
+    //This is id of HTML element (Khmer) with attribute lng-tag
+    $("#csTranslator").click(function(){
+        translate('cs');
+    });
+});
